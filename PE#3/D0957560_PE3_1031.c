c796ba6883bc80e29840dcfe4e27907ed6660ef8
IMGMANITSME <101241626+IMGMANITSME@users.noreply.github.com>
2022-10-31 23:18:21 +0800

    10/31 DSinC PE#3
    New version not better than old version, suck. :(

--- PE#3/D0957560_PE3_1031.c
+++ PE#3/D0957560_PE3_1031.c
@@ -5,6 +5,9 @@
 
 int global_clock = 0;
 int teller_amount = 0;
+int teller_count = 0;
+int last_arr_time[10] = {0};
+int last_ser_time[10] = {0};
 
 typedef struct Customer {
 	char name[10]; // «È¤á¦W¦r
@@ -27,7 +30,11 @@ TellerQueue teller_queue[10];
 
 void add(char*, int*, int*);
 
-int check();
+void close(int, int);
+
+void open(int, int);
+
+void check_leave();
 
 int main() {
 	FILE* input = fopen("input.txt","r");
@@ -36,14 +43,15 @@ int main() {
 	char temp[20];
 
 	fscanf(input,"%d",&teller_amount);
+	teller_count = teller_amount;
 
-	int i;
+	int i,j;
 
 	for(i = 0; i < teller_amount; i++) {
 		teller_queue[i].status = 2;
 		teller_queue[i].count = 0;
 		teller_queue[i].front = 0;
-		teller_queue[i].rear = 0;
+		teller_queue[i].rear = 1;
 		teller_queue[i].current_served_time = 0;
 	}
 
@@ -52,25 +60,86 @@ int main() {
 		fscanf(input,"%s %d %d",&C.name,&C.arr_time,&C.ser_time);
 
 		if(strcmp("#",C.name) == 0) {
-			printf("input == #\n");
+			int close_time = C.arr_time;
+			int close_teller = C.ser_time;
+			close(close_time,close_teller);
+			//printf("input == #\n");
 		} else if(strcmp("@",C.name) == 0) {
-			printf("input == @\n");
+			int open_time = C.arr_time;
+			int open_teller = C.ser_time;
+			open(open_time,open_teller);
+			//printf("input == @\n");
 		} else {
 			add(C.name,&C.arr_time,&C.ser_time);
-			printf("%s %d %d\n",C.name,C.arr_time,C.ser_time);
+			//printf("%s %d %d\n",C.name,C.arr_time,C.ser_time);
 		}
+		check_leave();
 	}
 
 	fclose(input);
 	fclose(output);
 }
 
-void add(char* a, int* b, int* c){
-	printf("%s %d %d\n",a,*b,*c);
+void add(char* a, int* b, int* c) {
+	global_clock = *b;
+	int i, j, mini;
+	if(teller_amount > 1) {
+		for(i = 0; i < teller_count - 1; i++) {//find the minimum count in all teller's customer queue
+			if(teller_queue[i].count <= teller_queue[i + 1].count) {
+				mini = i;
+			} else {
+				mini = i + 1;
+			}
+		}
+	} else {
+		mini = 0;
+	}
+	if(teller_queue[mini].count == 0) {
+		teller_queue[mini].current_served_time = 0;
+	} else {
+		teller_queue[mini].current_served_time = (last_ser_time[mini] + teller_queue[mini].current_served_time) - (*b - last_arr_time[mini]);
+	}
+	last_arr_time[mini]= *b;
+	last_ser_time[mini]= *c;
+	strcpy(teller_queue[mini].queue[teller_queue[mini].rear].name,a);
+	teller_queue[mini].queue[teller_queue[mini].rear].arr_time = *b + *c + teller_queue[mini].current_served_time;
+	teller_queue[mini].queue[teller_queue[mini].rear].ser_time = mini;
+	global_clock = teller_queue[mini].queue[teller_queue[mini].rear].arr_time;
+	teller_queue[mini].rear++;
+	teller_queue[mini].count++;
+
+	//printf("%s %d %d\n", a, *b, *c);
 }
 
-int check(){
-	int check_amount;
-	
+void close(int a, int b) {
+	int i,j;
+	while(a == global_clock) {
+		for(i = teller_queue[b].front + 2; i <= teller_queue[b].rear; i++ )
+			add(teller_queue[b].queue[i].name,&teller_queue[b].queue[i].arr_time,&teller_queue[b].queue[i].ser_time);
+		teller_queue[b].status = 0;
+		teller_queue[b].count = 0;
+		teller_queue[b].rear = 1;
+		teller_queue[b].front = 0;
+		teller_queue[b].current_served_time = 0;
+		teller_count--;
+	}
+}
+
+void open(int a, int b) {
+	while(a == global_clock) {
+		teller_queue[b].status = 2;
+		teller_queue[b].rear = 1;
+		teller_queue[b].front = 0;
+		teller_count++;
+	}
+}
+
+void check_leave(global_time) {
+	int i, j;
+	for(i = 0; i < teller_amount; i++) {
+		for(j = 1; j < teller_queue[i].rear; j++) {
+			printf("%s %d %d\n", teller_queue[i].queue[j].name, teller_queue[i].queue[j].arr_time ,teller_queue[i].queue[j].ser_time);
+		}
+	}
 }
 
